7daabbf805200accebc6d1c0a01b16047911ee62
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2023-11-10 08:37:15 +0530

    Added Components based service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { ExperienceComponentComponent } from './components/experience-component/experience-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponentComponent,
+    HeaderComponentComponent,
+    ExperienceComponentComponent
   ],
   imports: [
     BrowserModule,
